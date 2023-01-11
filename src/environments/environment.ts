81a1c9603994feac6118df8413bf8fc37a127bb5
ayushangular <ayushjdbc@gmail.com>
2023-01-11 13:11:03 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+ export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBwgfYZ5hcTRQiqAsk8iNr4ygLaGihYB7I",
+        authDomain: "firstproject-3510b.firebaseapp.com",
+        projectId: "firstproject-3510b",
+        storageBucket: "firstproject-3510b.appspot.com",
+        messagingSenderId: "1090814162332",
+        appId: "1:1090814162332:web:77ac412d2b35a8391ec459",
+        measurementId: "G-59RTVY1QGE"
+    },
+  };
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,40 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  // {
+  //   "hosting": {
+  //     "target": "kanban-fire",
+  //     "public": "dist/kanban-fire",
+  //     "ignore": [
+  //       "**/.*"
+  //     ],
+  //     "headers": [
+  //       {
+  //         "source": "*.[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f].+(css|js)",
+  //         "headers": [
+  //           {
+  //             "key": "Cache-Control",
+  //             "value": "public,max-age=31536000,immutable"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         "source": "/@(ngsw-worker.js|ngsw.json)",
+  //         "headers": [
+  //           {
+  //             "key": "Cache-Control",
+  //             "value": "no-cache"
+  //           }
+  //         ]
+  //       }
+  //     ],
+  //     "rewrites": [
+  //       {
+  //         "source": "**",
+  //         "destination": "/index.html"
+  //       }
+  //     ]
+  //   }
+  // }
